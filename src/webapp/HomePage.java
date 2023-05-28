7edc16e5232a8fec6abefe0970c9365c9fa01368
Flori_cyd <gaxherriflorian@gmail.com>
2023-05-29 00:11:36 +0200

    Home page updated with logo

---
+++ src/webapp/HomePage.java
@@ -0,0 +1,15 @@
+package webapp;
+
+public class HomePage {
+
+    // Setting up homepage
+
+    // nav menu
+
+    // only some changes to make in order for us to make the first commit and understand the process...
+
+    // more things for the homepage
+
+    //logo
+
+}
